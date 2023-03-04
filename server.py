04c9bb1dee6d3020ef8d1ae09906a2b25802b44a
Innovanito <khg05311@naver.com>
2023-03-04 19:49:51 +0900

    230304 before make update def

---
+++ server.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+nextId = 4
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javscript is ...'},
+]
+
+
+def template(contents, content, id=None):
+    contextUI = ''
+
+    if (id != None):
+        contextUI = f'''
+            <li><a href="/update/{id}/">update</a></li>
+        '''
+    return f"""<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol> 
+                {contents}
+            </ol>
+            {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+                {contextUI}
+            </ul>
+        </body>
+    </html>
+    """
+
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + \
+            f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+
+@app.route('/read/<int:id>/')
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if topic['id'] == id:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(),  f'<h2>{title}</h2>{body}', id)
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    print("request.method", request.method)
+    if (request.method == 'GET'):
+        # form 테그는 사용자가 입력한 정보를 서버로 전송하는 역할임
+        # form 태그를 이용해 정보를 수신하는 방법을 get방식이라고 함
+        content = """
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body" ></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        """
+        return template(getContents(), content)
+
+    elif request.method == "POST":
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)+'/'
+        nextId = nextId + 1
+
+        return redirect(url)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
